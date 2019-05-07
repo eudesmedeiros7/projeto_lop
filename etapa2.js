4b89b2ecb9f11bab166d5c798fea8cb680be407a
eudesmedeiros7 <50270620+eudesmedeiros7@users.noreply.github.com>
2019-05-07 11:30:00 -0300

    Add files via upload

---
+++ etapa2.js
@@ -0,0 +1,30 @@
+
+var x = 320;
+var y = 450;
+function setup() {
+	createCanvas(640, 480);
+}
+
+function draw() {
+  background(0);
+  if (keyIsDown(LEFT_ARROW))
+		x-=5;
+  if (keyIsDown(RIGHT_ARROW))
+		x+=5;
+  if (keyIsDown(UP_ARROW))
+		y-=5;
+  if (keyIsDown(DOWN_ARROW))
+		y+=5;
+  if (x>=640)
+		x = 0;
+  if (y>=450)
+		y=450;
+  if (x<0)
+      x = 640;
+  if (y<0)
+	  y = 480;
+		
+  ellipse(x, y, 50, 50);
+  rect(320,0,25,25);
+  
+}
